3350083763d4e946bdf391bda5301c5fbbe8b9d0
Николай Якунин <108213512+Nikolay-Yakunin@users.noreply.github.com>
2025-02-24 00:50:49 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,13 +11,13 @@ DEST_DIR="/usr/local/bin"
 echo "Building the binary..."
 make build-linux
 
-if [ ! -f "build/$BINARY_NAME" ]; then
+if [ ! -f "build/linux/$BINARY_NAME" ]; then
   echo "Failed to build the binary."
   exit 1
 fi
 
 echo "Copying binary to $DEST_DIR..."
-cp "build/$BINARY_NAME" "$DEST_DIR/"
+cp "build/linux/$BINARY_NAME" "$DEST_DIR/"
 
 if [ $? -eq 0 ]; then
   echo "Binary successfully installed to $DEST_DIR"
